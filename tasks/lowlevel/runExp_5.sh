034e74316d3e83a3628993f1e81891c2e931cbf3
Marcie <a.fuxjaeger@outlook.com>
2019-04-30 17:40:18 +0100

    update

--- tasks/lowlevel/runExp_5.sh
+++ tasks/lowlevel/runExp_5.sh
@@ -1,10 +1,10 @@
 
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_64_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 4 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_64_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 2 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_64_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 8 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_128_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 4 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_128_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 2 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_128_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 8 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_32_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 4 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_32_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 2 --num_epochs 200
-~/miniconda3/envs/mlp/bin/python3.7 ../../src/main.py --phase train --experiment_name ex_5_emnist_32_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 8 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_64_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 4 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_64_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 2 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_64_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 64 --categorical_dim 8 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_128_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 4 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_128_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 2 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_128_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 8 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_32_4 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 4 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_32_2 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 2 --num_epochs 200
+~/miniconda3/envs/mlp/bin/python3.7 ../../src/lowlevel/main.py --phase train --experiment_name ex_5_emnist_32_8 --dataset emnist --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 8 --num_epochs 200
