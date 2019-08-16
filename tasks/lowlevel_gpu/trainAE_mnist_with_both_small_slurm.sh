a7d279361d7228abae2659664ed3bffc25eea0a1
a.fuxjaeger@outlook.com <a.fuxjaeger@outlook.com>
2019-08-16 15:53:26 +0100

    update

--- tasks/lowlevel_gpu/trainAE_mnist_with_both_small_slurm.sh
+++ tasks/lowlevel_gpu/trainAE_mnist_with_both_small_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --job-name=job_exp_dropout_both
-#SBATCH --output=job_exp_dropout_both.txt
+#SBATCH --job-name=job_exp_dropout_both_small
+#SBATCH --output=job_exp_dropout_both_small.txt
 #
 #SBATCH --ntasks=1
 #SBATCH --mem=32G
@@ -10,4 +10,4 @@
 
 
 source activate mlp
-python ../../src/lowlevel/main.py --phase train --experiment_name exp_dropout_both --dataset mnist --gpu_ids 0,1 --feature_layer_size 16 --categorical_dim 2 --num_epochs 400 --ae_model_type vanilla --use_dropout_decoder True --use_dropout_encoder True
+python ../../src/lowlevel/main.py --phase train --experiment_name exp_dropout_both_small --dataset mnist --gpu_ids 0,1 --feature_layer_size 16 --categorical_dim 2 --num_epochs 400 --ae_model_type vanilla --use_dropout_decoder True --use_dropout_encoder True
