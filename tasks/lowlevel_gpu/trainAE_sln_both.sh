cb65ec91c747bb696c223a4c81aea660969dbe2f
Marcie <a.fuxjaeger@outlook.com>
2019-08-18 23:23:13 +0100

    update

--- tasks/lowlevel_gpu/trainAE_sln_both.sh
+++ tasks/lowlevel_gpu/trainAE_sln_both.sh
@@ -1,1 +1,13 @@
-python ../../src/lowlevel/main.py --phase train --experiment_name exp_sln_test --dataset sln --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 2 --num_epochs 50 --batch_size 10 --ae_model_type vanilla --testing True
+#!/bin/bash
+#
+#SBATCH --job-name=job_exp_sln_both
+#SBATCH --output=job_exp_sln_both.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:2
+#SBATCH --time=24:00:00
+
+
+source activate mlp
+python ../../src/lowlevel/main.py --phase train --experiment_name exp_sln_both --dataset sln --gpu_ids 0,1 --feature_layer_size 32 --categorical_dim 2 --num_epochs 500 --batch_size 10 --ae_model_type vanilla
