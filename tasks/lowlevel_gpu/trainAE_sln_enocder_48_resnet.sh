9382f0b3bbd068299c84683c747fa15fdb5023c6
a.fuxjaeger@outlook.com <a.fuxjaeger@outlook.com>
2019-08-20 18:02:03 +0100

    update

--- tasks/lowlevel_gpu/trainAE_sln_enocder_48_resnet.sh
+++ tasks/lowlevel_gpu/trainAE_sln_enocder_48_resnet.sh
@@ -10,4 +10,4 @@
 
 
 source activate mlp
-python ../../src/lowlevel/main.py --phase train --experiment_name exp_sln_encoder_only_48_resnet --dataset sln --gpu_ids 0,1 --feature_layer_size 48 --categorical_dim 2 --num_epochs 500 --batch_size 10 --ae_model_type resnet --use_dropout_decoder False --testing True
+python ../../src/lowlevel/main.py --phase train --experiment_name exp_sln_encoder_only_48_resnet --dataset sln --gpu_ids 0,1 --feature_layer_size 48 --categorical_dim 2 --num_epochs 500 --batch_size 10 --ae_model_type resnet --use_dropout_decoder False
