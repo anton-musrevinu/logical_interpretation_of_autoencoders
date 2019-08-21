01d84ad3f217717459b138cb2b4991dcb2678e63
a.fuxjaeger@outlook.com <a.fuxjaeger@outlook.com>
2019-08-21 16:45:41 +0100

    update

---
+++ tasks/lowlevel_gpu/trainAE_sln_enocder_128_deep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=job_exp_sln_encoder_only_128_deep
+#SBATCH --output=job_exp_sln_encoder_only_128_deep.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:2
+#SBATCH --time=96:00:00
+
+
+source activate mlp
+python ../../src/lowlevel/main.py --phase train --experiment_name exp_sln_encoder_only_128_deep --dataset sln --gpu_ids 0,1 --feature_layer_size 128 --categorical_dim 2 --num_epochs 500 --batch_size 10 --ae_model_type deep --use_dropout_decoder False
